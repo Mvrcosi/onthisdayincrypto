6eb1d91427f149b1adee0e036f32be36e6065ce2
Mvrcosi <saucedoimarcos@gmail.com>
2022-04-04 18:33:17 -0700

    simple navbar built using tailwind

---
+++ onthisdayincrypto/src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header >
+            <nav className='container flex justify-between  bg-slate-700 text-stone-50		'>
+                <div >
+                    <p className='text-[44px] tracking-wide	' >Onthisdayin<span className='font-thin'>crypto</span></p>
+                </div>
+                <div className='my-5'>
+                    <button>settings</button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ onthisdayincrypto/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: '1300px',
+        xl: '1300px',
+        "2xl": '1300px'
+      }
+    }
+  },
+  plugins: [],
+}
